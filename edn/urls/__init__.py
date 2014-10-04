692b6b415ad0d5440b0a767eaad74d7be3ad1c93
Carl Marshall <carl@cm13.net>
2014-10-04 13:03:30 +0100

    Bug: Missed out including the urls

--- edn/urls/__init__.py
+++ edn/urls/__init__.py
@@ -1,1 +1,14 @@
-__author__ = 'carl'
+"""
+Default urls for the edn
+"""
+from django.conf.urls import include
+from django.conf.urls import patterns
+from django.conf.urls import url
+
+urlpatterns = patterns(
+    '',
+    url(r'^mailing/', include('edn.urls.mailing_list')),
+    url(r'^tracking/', include('edn.urls.tracking')),
+    url(r'^statistics/', include('edn.urls.statistics')),
+    url(r'^', include('edn.urls.newsletter')),
+)
